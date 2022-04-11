1ed0deedb73e24c5e77bc73d438592ac313fbb45
g00dvin <vladislav.shiller@gmail.com>
2022-04-11 16:36:38 +0300

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,6 @@
+fun main() {
+    val likes = "56"
+
+    if (likes.get(likes.length - 1) == '1') println("Понравилось " + likes + " человеку")
+    else println("Понравилось " + likes + " человекам")
+}
